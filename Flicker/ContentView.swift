2e226c77bbefbad929a48d69f47081787d28123a
Vahid Sayad <vahidsayad@gmail.com>
2022-05-27 12:40:00 +0430

    Initial Commit

---
+++ Flicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flicker
+//
+//  Created by Vahid Sayad on 27/5/2022 .
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flicker/FlickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerApp.swift
+//  Flicker
+//
+//  Created by Vahid Sayad on 27/5/2022 .
+//
+
+import SwiftUI
+
+@main
+struct FlickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
